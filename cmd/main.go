c39f082acf2bdd277c952d6c8685a1fc63f2cb85
sanzhar857 <szhubatkhanov@gmail.com>
2026-03-10 23:59:13 +0500

    add main and mod file

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
-	
+
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("The required format to continue working: ./task-tracker [command] [arguments]")
+		fmt.Println("Or enter a command to view a list of available commands.")
+		return
+	}
+
+	command := args[0]
+	switch strings.TrimSpace(command) {
+	case "add":
+	case "update":
+	case "delete":
+	case "list":
+	default:
+		fmt.Println("Unknown command.")
+	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sanzhar857/task-tracker
+
+go 1.25.6
